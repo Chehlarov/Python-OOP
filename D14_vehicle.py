0d3b22ef3fa5ab3683b48b80742aa78ef9b0b6c2
Nikolay Chehlarov <78173632+Chehlarov@users.noreply.github.com>
2021-03-20 21:31:02 +0200

    Add files via upload

--- D14_vehicle.py
+++ D14_vehicle.py
@@ -6,36 +6,32 @@ class Vehicle(ABC):
         self.fuel_quantity = fuel_quantity
         self.fuel_consumption = fuel_consumption
 
+    @property
     @abstractmethod
-    def drive(self, distance):
-        pass
+    def _AC_INCREASE(self):
+        raise NotImplementedError
 
+    @property
     @abstractmethod
+    def _REFUEL_EFFICIENCY(self):
+        raise NotImplementedError
+
+    def drive(self, distance):
+        if self.fuel_quantity >= distance * (self.fuel_consumption + self._AC_INCREASE):
+            self.fuel_quantity -= distance * (self.fuel_consumption + self._AC_INCREASE)
+
     def refuel(self, fuel):
-        pass
+        self.fuel_quantity += fuel * self._REFUEL_EFFICIENCY
 
 
 class Car(Vehicle):
-    AC_INCREASE = 0.9
-
-    def drive(self, distance):
-        if self.fuel_quantity >= distance * (self.fuel_consumption + self.AC_INCREASE):
-            self.fuel_quantity -= distance * (self.fuel_consumption + self.AC_INCREASE)
-
-    def refuel(self, fuel):
-        self.fuel_quantity += fuel
+    _AC_INCREASE = 0.9
+    _REFUEL_EFFICIENCY = 1.0
 
 
 class Truck(Vehicle):
-    AC_INCREASE = 1.6
-    REFUEL_EFFICIENCY = 0.95
-
-    def drive(self, distance):
-        if self.fuel_quantity >= distance * (self.fuel_consumption + self.AC_INCREASE):
-            self.fuel_quantity -= distance * (self.fuel_consumption + self.AC_INCREASE)
-
-    def refuel(self, fuel):
-        self.fuel_quantity += fuel * self.REFUEL_EFFICIENCY
+    _AC_INCREASE = 1.6
+    _REFUEL_EFFICIENCY = 0.95
 
 
 truck = Truck(100, 15)
@@ -43,4 +39,3 @@ truck.drive(5)
 print(truck.fuel_quantity)
 truck.refuel(50)
 print(truck.fuel_quantity)
-
